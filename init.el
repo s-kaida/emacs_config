fc5c74f3bbb4615ba798bfbf0c71aae2dd1566df
s-kaida <color.8kk@gmail.com>
2019-04-03 22:54:21 +0900

    add init.el

---
+++ init.el
@@ -0,0 +1,16 @@
+
+;; スタートアップメッセージを表示させない
+(setq inhibit-startup-message t)
+
+
+;; タブにスペースを使用する
+(setq-default tab-width 4 indent-tabs-mode nil)
+
+;; 行数を表示する
+(global-linum-mode t)
+
+;; スクロールを１行にごとする
+(setq scroll-conservatively 1)
+
+;; 対応する括弧をハイライトする
+(show-paren-mode 1)
